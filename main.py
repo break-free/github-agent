b1ad41b5d5315c416f6806f13b98ff7b2b1e5073
Zak Alford <zalford@breakfreesolutions.com>
2023-08-07 14:55:52 -0500

    cleaning up print statements

--- main.py
+++ main.py
@@ -27,15 +27,22 @@ for var in env_vars:
     else: # Complaint line
         raise Exception(f"The environment variable {var} was not set. You must set this value to continue.")
     
-print(os.environ["GITHUB_REPOSITORY"])
-
 llm = OpenAI(temperature=0)
 github = GitHubAPIWrapper()
 toolkit = GitHubToolkit.from_github_api_wrapper(github)
+
+tools = []
+unwanted_tools = ['Get Issue','Delete File', 'Create File']
+
+for tool in toolkit.get_tools():
+    if tool.name not in unwanted_tools:
+        tools.append(tool)
+
 agent = initialize_agent(
-    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
-agent.run(
-    "You have the software engineering capabilities of a Google Principle engineer. You are tasked with completing issues on a github repository. Please look at the existing issues and complete them."
-)
+# request = "You have the software engineering capabilities of a Google Principle engineer. You are tasked with completing issues on a github repository. Please look at the existing issues and complete them."
+request = "Find relevant Github Application Documentation and place their documentation in the 'References' section of the root README.md file. Open a pull request into main for any changes."
+
+agent.run(request)
