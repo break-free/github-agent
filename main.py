ca341b83871ccd9215525d02050641959947e607
Zak <zalford@breakfreesolutions.com>
2023-08-09 10:25:00 -0500

    testing different prompts

--- main.py
+++ main.py
@@ -29,26 +29,29 @@ for var in env_vars:
     else: # Complaint line
         raise Exception(f"The environment variable {var} was not set. You must set this value to continue.")
     
+bot_branch = os.environ["GITHUB_BRANCH"]
 gh_base_branch = os.environ["GITHUB_BASE_BRANCH"]
     
 llm = ChatOpenAI(model="gpt-4")
-# llm = OpenAI(temperature=0,model="gpt-4")
 github = GitHubAPIWrapper()
 toolkit = GitHubToolkit.from_github_api_wrapper(github)
 
 tools = []
 
-unwanted_tools = ['Get Issue','Delete File']
+# unwanted_tools = ['Get Issue','Delete File']
+unwanted_tools = []
 for tool in toolkit.get_tools():
     if tool.name not in unwanted_tools:
         tools.append(tool)
 
-tools.append(custom_tools.search_online)
+# tools.append(custom_tools.search_online)
 
 agent = initialize_agent(
     tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
-request = f"Search online for documentation about Github Applications and place their links in the 'References' section of the root README.md file. Open a pull request into {gh_base_branch} for any changes."
+# request = f"Search online for documentation about Github Applications and place their links in the 'References' section of the root README.md file. Open a pull request into {gh_base_branch} for any changes."
+request = f"Search online for documentation about Artificial Intelligence and place their links in the 'References' section of the root README.md file. Open a pull request into {gh_base_branch} for any changes."
+# request = f"Start a new git branch from {bot_branch}. Then, search online for documentation about macaroni & cheese and place their links in the 'Culinary' section of the root README.md file. Open a pull request from your new feature branch into {gh_base_branch} for any changes."
 
 agent.run(request)
